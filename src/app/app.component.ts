486428f31ff4a71e86acb5b52d4078c1546bcbe8
Francisco do Vale <fdovalejr@gmail.com>
2020-09-30 09:10:42 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'calendar-app-angular';
+}
